26227ce613c9506aadf962dd7368d99b3f6a51c1
57Anonymous34 <A.Jashari.inf24@stud.bbbaden.ch>
2024-09-13 11:35:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Zahlenraten_Spiel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lösung :");
+            Console.WriteLine("Addieren und Multi");
+            int a = 525;
+            int b = 320;
+            int c = 54;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            // Subraction
+            Console.WriteLine("Subtraction");
+            int e = 53;
+            int f = 75;
+            int g = e - f;
+            Console.WriteLine(g);
+
+            //Dividieren
+            Console.WriteLine("Dividerung");
+            int h = 53;
+            int i = 6;
+            int j = h / i;
+            Console.WriteLine(j);
+
+            //Multi
+            Console.WriteLine("Multi");
+            int k = 25;
+            int l = 60;
+            int m = 34;
+            int n = k * l * m;
+            Console.WriteLine(n);
+
+
+        }
+    }
+}
