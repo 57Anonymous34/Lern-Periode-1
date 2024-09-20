bdb156d2d19a35888da5ff1be4066e881c3c6243
57Anonymous34 <A.Jashari.inf24@stud.bbbaden.ch>
2024-09-20 11:45:12 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,35 +4,102 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Lösung :");
-            Console.WriteLine("Addieren und Multi");
-            int a = 525;
-            int b = 320;
-            int c = 54;
-            int d = a + b * c;
-            Console.WriteLine(d);
+            // -Addieren-
 
-            // Subraction
-            Console.WriteLine("Subtraction");
-            int e = 53;
-            int f = 75;
-            int g = e - f;
-            Console.WriteLine(g);
+            Console.WriteLine("___Addierung___");
+            Console.WriteLine("Zahl1:");
+            string Aigel = Console.ReadLine();
+            double a = Convert.ToDouble(Aigel);
+            
+            Console.WriteLine("Zahl2:");
+            string Bigel = Console.ReadLine();
+            double b = Convert.ToDouble(Bigel); 
 
-            //Dividieren
-            Console.WriteLine("Dividerung");
-            int h = 53;
-            int i = 6;
-            int j = h / i;
-            Console.WriteLine(j);
+            Console.WriteLine("Zahl3:");
+            string Cigel = Console.ReadLine();
+            double c = Convert.ToDouble(Cigel);
 
-            //Multi
-            Console.WriteLine("Multi");
-            int k = 25;
-            int l = 60;
-            int m = 34;
-            int n = k * l * m;
-            Console.WriteLine(n);
+            // Verarbeitung
+            double d = a + b + c;
+            Console.WriteLine($"-Lösungen:{d}");
+
+            
+            
+            
+            // -Subraction-
+            
+            Console.WriteLine("\n___Subtraction___");
+            Console.WriteLine("Zahl1:");
+            string Aagel = Console.ReadLine();
+            double e = Convert.ToDouble(Aagel);
+
+            Console.WriteLine("Zahl2:");
+            string Bagel = Console.ReadLine();
+            double f = Convert.ToDouble(Bagel);
+            
+            // Verarbeitung
+            double g = e - f;
+            Console.WriteLine($"-lösungen:{g}");
+
+            
+           
+            
+            // -Dividieren-
+           
+            Console.WriteLine("\n___Dividerung___");
+            Console.WriteLine("Zahl1:");
+            string Auto = Console.ReadLine();
+            double h = Convert.ToDouble(Auto);
+
+            Console.WriteLine("Zahl2:");
+            string Buto = Console.ReadLine();
+            double i= Convert.ToDouble(Buto);
+            // Verarbeitung
+            double j = h / i;
+            Console.WriteLine($"-Lösung:{j}");
+
+            
+            
+         
+
+            // -Multiplikation-
+            
+            Console.WriteLine("\n___Multiplikation___");
+            Console.WriteLine("Zahl1:");
+            string Aingabe = Console.ReadLine();
+            double k = Convert.ToDouble(Aingabe);
+
+            Console.WriteLine("Zahl2:");
+            string Bingabe = Console.ReadLine();
+            double l = Convert.ToDouble(Bingabe);
+
+            Console.WriteLine("Zahl3:");
+            string Cingabe = Console.ReadLine();
+            double m = Convert.ToDouble(Cingabe);
+           
+            // Verarbeitung
+            double n = k * l * m;
+            Console.WriteLine($"-Lösung:{n}");
+
+
+
+
+            //Notendurchschnitt
+            Console.WriteLine("\n\nNotenberechner");
+            Console.WriteLine("\nGeben ihre Note ein ein:");
+            string eingabe = Console.ReadLine();
+            double Note1 = Convert.ToDouble(eingabe);
+
+            Console.WriteLine("Geben sien die andere Note ein:");
+            string Zahl1 = Console.ReadLine();
+            double Note2 = Convert.ToDouble(Zahl1);
+
+
+            // Verarbeitung
+            double rechnung = (Note1 + Note2 );
+            double Resultat = (rechnung / 2);
+
+            Console.WriteLine($"Ihr durchschnitt beträgt:{Resultat}");
 
 
         }
